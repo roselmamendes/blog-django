176bbe2c33e0fdf220f7acf20485554f38153233
Roselma Mendes <roselma.mendes@gmail.com>
2016-01-23 23:47:05 -0300

    Fix flake8 warning.

--- blog/posts/tests.py
+++ blog/posts/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from posts.creation_post import CreationPost
-from posts.models import Post, Comment
+from posts.models import Post
 
 
 class CreationPostTest(TestCase):
